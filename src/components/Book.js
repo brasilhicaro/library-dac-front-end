58933568aca52184574f8971adc41360ec2e6eaf
hicaro_brasil <hicaro.brasil@hotmail.com>
2023-09-10 03:56:53 -0300

    :construction: build: html structure

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,7 +1,26 @@
 
 function Book(){
     return (
-      <h1> Book</h1>
+      <div className="container">
+        <div className="container-book">
+          <div className="wrap-book">
+            <form className="form-book">
+              <span className="book-form-title">Gerar Livro</span>
+              <div className="container-book-form-btn">
+                <span className="book-form-title-input">Nome do Livro</span>
+                <input type="text" className="book-form-input" />
+                <span className="book-form-title-input">Ano de Publicação</span>
+                <input type="text" className="book-form-input" />
+                <span className="book-form-title-input">Editora</span>
+                <input type="text" className="book-form-input" />
+                <span className="book-form-title-input">Autor</span>
+                <input type="text" className="book-form-input" />
+                <button className="book-form-btn"  type="button">Cadastrar Livro</button>
+              </div>
+            </form>
+          </div>      
+        </div>
+      </div>
     );
 }
 
