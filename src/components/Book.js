4146673775f23612c486669de8e30ea8997a836a
hicaro_brasil <hicaro.brasil@hotmail.com>
2023-09-10 03:36:15 -0300

    :heavy_plus_sign: build: add components

---
+++ src/components/Book.js
@@ -0,0 +1,8 @@
+
+function Book(){
+    return (
+      <h1> Book</h1>
+    );
+}
+
+export default Book;
---
+++ src/components/Reserve.js
@@ -0,0 +1,6 @@
+
+function Reserve() {
+   return( <h1>Reserve</h1>);
+}
+
+export default Reserve;
