8c1c2819ec14d318779ae5da73d0791d9d6bf21f
hicaro_brasil <hicaro.brasil@hotmail.com>
2023-09-10 04:02:30 -0300

    :construction: build: html structure

--- src/components/Reserve.js
+++ src/components/Reserve.js
@@ -1,6 +1,25 @@
 
 function Reserve() {
-   return( <h1>Reserve</h1>);
+<div className="container">
+   <div className="container-reserve">
+     <div className="wrap-reserve">
+       <form className="form-reserve">
+         <span className="reserve-form-title">Gerar Reserva</span>
+         <div className="container-reserve-form-btn">
+           <span className="reserve-form-title-input">Responsável</span>
+           <input type="text" className="reserve-form-input" />
+           <span className="reserve-form-title-input">Data de Emprestimo</span>
+           <input type="text" className="reserve-form-input" />
+           <span className="reserve-form-title-input">Data de Devolução</span>
+           <input type="text" className="reserve-form-input" />
+           <span className="reserve-form-title-input">Código do Livro</span>
+           <input type="text" className="reserve-form-input" />
+           <button className="reserve-form-btn"  type="button">Cadastrar Reserva</button>
+         </div>
+       </form>
+     </div>      
+   </div>
+ </div>
 }
 
 export default Reserve;
