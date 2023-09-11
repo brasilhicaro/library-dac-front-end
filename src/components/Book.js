8bc5c5905d68049f169e69fd198f87e3a5005042
hicaro_brasil <hicaro.brasil@hotmail.com>
2023-09-10 21:27:05 -0300

    :sparkles: feat: finish book

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,6 +1,14 @@
 import './styles/Book.css'
+import '../App.css'
+import { useState } from 'react';
 
 function Book(){
+
+  const[bookName, setBook] = useState('');
+  const[publisher, setPublisher] = useState('');
+  const[year, setYear] = useState('');
+  const[author, setAuthor] = useState('');
+
     return (
       <div className="container">
         <div className="container-book">
@@ -10,22 +18,42 @@ function Book(){
               <div className="container-book-form-btn">
 
                 <div className="wrap-input">
-                <input type="text" className="book-form-input book-input" />
+                <input
+                type="text" 
+                className={bookName.length > 0 ? "has-value book-form-input book-input" : "book-form-input"}
+                value={bookName}
+                onChange={(event) => setBook(event.target.value)}
+                />
                 <span className="book-form-title-input" data-placeholder='Nome do Livro'></span>
                 </div>
                 
                 <div className="wrap-input">
-                  <input type="text" className="book-form-input book-input" />
+                  <input 
+                  type="text" 
+                  className={publisher.length > 0 ? "has-value book-form-input book-input" : "book-form-input"}
+                  value={publisher}
+                  onChange={(event) => setPublisher(event.target.value)}
+                  />
                   <span className="book-form-title-input" data-placeholder='Editora'></span>
                 </div>
 
                 <div className="wrap-input">
-                  <input type="text" className="book-form-input book-input" />
+                  <input
+                  type="text"
+                  className={year.length > 0 ? "has-value book-form-input book-input" : "book-form-input"}
+                  value={year}
+                  onChange={(event) => setYear(event.target.value)}
+                  />
                   <span className="book-form-title-input" data-placeholder='Ano de Publicação'></span>
                 </div>
 
                 <div className="wrap-input">
-                  <input type="text" className="book-form-input book-input" />
+                  <input 
+                  type="text" 
+                  className={author.length > 0 ? "has-value book-form-input book-input" : "book-form-input"}
+                  value={author}
+                  onChange={(event) => setAuthor(event.target.value)}
+                  />
                   <span className="book-form-title-input" data-placeholder='Autor'></span>
                 </div>
 
